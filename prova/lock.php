7338f46dcd428fc83d0dceff12c6ba9d5259bbf3
tamirveiga <166255386+tamirveiga@users.noreply.github.com>
2025-06-11 22:20:49 -0300

    Delete prova directory

--- prova/lock.php
+++
@@ -1,7 +0,0 @@
-<?php session_start();
-    if (!isset($_SESSION['id']) ||
-        !isset($_SESSION['usuario']) || 
-        !isset($_SESSION['senha'])) {
-        header('location:index.php?codigo=1');
-    }
-?>
